207954f431b02b797a1299292e66135fd29e312b
xl-Mu <xmz140601@gmail.com>
2023-11-24 22:38:51 +0800

    updated basic components

---
+++ frontend/src/components/Content.js
@@ -0,0 +1,12 @@
+// Content.js
+function Content() {
+    return (
+        <div className="p-4">
+            <div className="text-center text-gray-400">
+                0 items {/* This would be dynamic based on the content */}
+            </div>
+        </div>
+    );
+}
+
+export default Content;
