e9c7e7f0f9085d4025b394b218daff6a4adce9f4
fish-r <gyf8800@gmail.com>
2023-12-05 03:14:47 +0800

    feature: able to get selectedAddr

--- frontend/src/mantineComponents/HeaderMegaMenu.js
+++ frontend/src/mantineComponents/HeaderMegaMenu.js
@@ -32,6 +32,9 @@ import {
 } from '@tabler/icons-react';
 import classes from './HeaderMegaMenu.module.css';
 import { useState } from 'react';
+import { ethers } from "ethers";
+import TokenArtifact from "../contracts/Token.json";
+import contractAddress from "../contracts/contract-address.json";
 
 const mockdata = [
     {
@@ -65,37 +68,9 @@ const mockdata = [
         description: 'Combusken battles with the intensely hot flames it spews',
     },
 ];
-const connectWallet = async () => {
-    // This method is run when the user clicks the Connect. It connects the
-    // dapp to the user's wallet, and initializes it.
 
-    // To connect to the user's wallet, we have to run this method.
-    // It returns a promise that will resolve to the user's address.
-    const [selectedAddress] = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-    // Once we have the address, we can initialize the application.
-
-    // First we check the network
-    this._checkNetwork();
-
-    this._initialize(selectedAddress);
-
-    // We reinitialize it whenever the user changes their account.
-    window.ethereum.on("accountsChanged", ([newAddress]) => {
-        this._stopPollingData();
-        // `accountsChanged` event can be triggered with an undefined newAddress.
-        // This happens when the user removes the Dapp from the "Connected
-        // list of sites allowed access to your addresses" (Metamask > Settings > Connections)
-        // To avoid errors, we reset the dapp state 
-        if (newAddress === undefined) {
-            return this._resetState();
-        }
-
-        this._initialize(newAddress);
-    });
-}
-
-const intialState = {
+const initialState = {
     // The info of the token (i.e. It's Name and symbol)
     tokenData: undefined,
     // The user's address and balance
@@ -111,8 +86,97 @@ const intialState = {
 export function HeaderMegaMenu() {
     const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
     const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-    const [loginState, setLoginState] = useState(intialState)
+    const [loginState, setLoginState] = useState(initialState)
     const theme = useMantineTheme();
+    var token;
+
+
+    const connectWallet = async () => {
+        console.log('conn')
+        const [selectedAddress] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('selected addr', selectedAddress)
+        // Once we have the address, we can initialize the application.
+
+        // First we check the network
+        // this._checkNetwork();
+
+        initialize(selectedAddress);
+
+        // We reinitialize it whenever the user changes their account.
+        window.ethereum.on("accountsChanged", ([newAddress]) => {
+            stopPollingData();
+            // `accountsChanged` event can be triggered with an undefined newAddress.
+            // This happens when the user removes the Dapp from the "Connected
+            // list of sites allowed access to your addresses" (Metamask > Settings > Connections)
+            // To avoid errors, we reset the dapp state 
+            if (newAddress === undefined) {
+                return resetState();
+            }
+
+            initialize(newAddress);
+        });
+    }
+    const initialize = (userAddress) => {
+        console.log('init', userAddress)
+        // We first store the user's address in the component's state
+
+        setLoginState({ ...loginState, selectedAddress: userAddress })
+
+        // Then, we initialize ethers, fetch the token's data, and start polling
+        // for the user's balance.
+
+        // Fetching the token data and the user's balance are specific to this
+        // sample project, but you can reuse the same initialization pattern.
+        initializeEthers();
+        getTokenData();
+        startPollingData();
+    }
+
+    const initializeEthers = async () => {
+        // We first initialize ethers by creating a provider using window.ethereum
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        // Then, we initialize the contract using that provider and the token's
+        // artifact. You can do this same thing with your contracts.
+        token = new ethers.Contract(
+            contractAddress.Token,
+            TokenArtifact.abi,
+            provider.getSigner(0)
+        );
+
+        console.log('token', token)
+    }
+
+
+    const startPollingData = () => {
+        const pollDataInterval = setInterval(() => this._updateBalance(), 1000);
+        updateBalance();
+    }
+
+    const stopPollingData = () => {
+        clearInterval(this._pollDataInterval);
+        const pollDataInterval = undefined;
+    }
+
+
+    const getTokenData = async () => {
+        const name = await this._token.name();
+        const symbol = await this._token.symbol();
+
+        setLoginState({ ...loginState, tokenData: { name, symbol } });
+    }
+
+    const updateBalance = async () => {
+        const balance = await this._token.balanceOf(this.state.selectedAddress);
+        this.setState({ balance });
+    }
+
+    // This method resets the state
+    const resetState = () => {
+        setLoginState(initialState);
+    }
+
+
 
 
 
@@ -199,13 +263,13 @@ export function HeaderMegaMenu() {
                     <Group visibleFrom="sm">
                         {loginState.selectedAddress ?
 
-                            <Button id={loginState} variant='default' onClick={() => { console.log('profile') }}>Profile</Button>
+                            <Button id={loginState} variant='default' onClick={() => { window.alert(JSON.stringify(loginState)) }}>Profile</Button>
 
                             : <Button id={loginState} variant="default" onClick={() => {
                                 console.log('Log in')
                                 connectWallet()
                             }}>Log in</Button>}
-                        <Button onClick={() => { console.log(loginState.balance) }}>My Cart</Button>
+                        <Button onClick={() => { console.log(loginState) }}>My Cart</Button>
                     </Group>
 
                     <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
