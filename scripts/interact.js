0376738606c5a89afc6dc0ce046bdeb2007d922d
fish-r <gyf8800@gmail.com>
2023-12-05 23:37:04 +0800

    fix: fixed interact.js not writing to block

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,7 @@
 // scripts/interact.js
 async function main() {
   const MyContract = await ethers.getContractFactory('MusicCopyrightMarketplace');
-  const myContract = await MyContract.attach('0xe7f1725e7734ce288f8367e1bb143e90bb3f0512');
+  const contract = await MyContract.attach('0xe7f1725e7734ce288f8367e1bb143e90bb3f0512');
 
   const musicList = [];
   for (index = 0; index < 10; index += 1) {
@@ -12,46 +12,22 @@ async function main() {
       title: `title ${index}`,
       price: index
     })
-    console.log("Listed Music: ", listMusic);
-
-    const musicCopyright = await myContract.getMusicCopyright(1);
-
-    console.log("Music copyright id 1", musicCopyright);
-
-    const mappingKeys = await myContract.getMappingKeys();
-
-    console.log("All Music copyright", mappingKeys[0]);
   }
 
-  const obj = {
-    id: 1,
-    image_url: 'https://t4.ftcdn.net/jpg/00/10/33/17/360_F_10331779_PVOLBM8MIeDZW9H0vc3Cr0nLMoSEO8Le.jpg',
-    artist_name: `artist${1}`,
-    title: `title ${1}`,
-    price: 3
-  }
-  // musicList.map(async (each) => {
-  //   const listMusic = await myContract.listMusicCopyright(
-  //     each.id, each.image_url, each.artist_name, each.title, each.price
-  //   )
-  //   console.log(listMusic)
-  // })
+  const test = musicList.map(async (each) => {
+    const listMusic = await contract.listMusicCopyright(
+      each.id, each.image_url, each.artist_name, each.title, each.price
+    )
+    return listMusic
+  })
 
-  const listMusic = await myContract.listMusicCopyright(
-    obj.id, obj.image_url, obj.artist_name, obj.title, obj.price
-  )
-  console.log(listMusic)
-  // Call contract functions or perform interactions here
-  // const listMusic = await myContract.listMusicCopyright(1, "www.drive.com", "Justin Bieber", "Sad", 2);
+  const res = await Promise.all(test)
+  console.log(res)
 
-  // console.log("Listed Music: ", listMusic);
 
-  // const musicCopyright = await myContract.getMusicCopyright(1);
 
-  // console.log("Music copyright id 1", musicCopyright);
-
-  const allmusicCopyright = await myContract.getMappingKeys();
-  // const single = await myContract.getMusicCopyright(1);
+  const allmusicCopyright = await contract.getMappingKeys();
+  // const single = await contract.getMusicCopyright(1);
 
 
   console.log("All Music copyright", allmusicCopyright);
